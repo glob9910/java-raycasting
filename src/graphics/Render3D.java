4a1335b9773040bd6b9710810f18355be717c8a5
kamila <268265@student.pwr.edu.pl>
2024-06-27 11:36:59 +0200

    render distance

--- src/graphics/Render3D.java
+++ src/graphics/Render3D.java
@@ -4,8 +4,12 @@ import logic.Game;
 
 public class Render3D extends Render {
 
+    private int renderDistance = 3000;
+    private double[] zBuffer;
+
     public Render3D(int width, int height) {
         super(width, height);
+        zBuffer = new double[width*height];
     }
 
     // Notch's technique:
@@ -21,23 +25,51 @@ public class Render3D extends Render {
         double sine = Math.sin(rotation);
 
         for(int y=0; y<height; y++) {
-            double ceiling = (y - height/2.0)/height;
+            double ceiling = (y - height/2.0) / height;
 
-            double z = floorPosition/ceiling;
+            double z = floorPosition / ceiling;
 
             if(ceiling < 0) {
-                z = ceilingPosition/-ceiling;
+                z = ceilingPosition / -ceiling;
             }
 
             for(int x=0; x<width; x++) {
                 double depth = (x - width/2.0)/height;
                 depth *= z;
-                double xx = depth*cosine + z*sine + right;
-                double yy = z*cosine - depth*sine + forward;
-                int xPix = (int)xx;
-                int yPix = (int)yy;
+                double xx = depth*cosine + z*sine;
+                double yy = z*cosine - depth*sine;
+                int xPix = (int)(xx + right);
+                int yPix = (int)(yy + forward);
+                zBuffer[x + y*width] = z;
                 pixels[x + y*width] = (((xPix & 15) * 16) | ((yPix & 15) * 16)) << 1;   // 8 - green, 16 - red, 0 - blue
+
+                if(z > 500) {
+                    pixels[x + y*width] = 0;
+                }
             }
+        }
+    }
+
+    public void renderDistanceLimiter() {
+        for(int i=0; i<width*height; i++) {
+            int color = pixels[i];
+            int brightness = (int)(renderDistance/zBuffer[i]);
+
+            // brightness (0-255)
+            if(brightness < 0)
+                brightness = 0;
+            if(brightness > 255)
+                brightness = 255;
+
+            int r = (color >> 16) & 0xff;   // <3
+            int g = (color >> 8) & 0xff;
+            int b = color & 0xff;
+
+            r = r*brightness/255;
+            g = g*brightness/255;
+            b = b*brightness/255;
+
+            pixels[i] = r << 16 | g << 8 | b;
         }
     }
 }
--- src/graphics/Screen.java
+++ src/graphics/Screen.java
@@ -23,6 +23,7 @@ public class Screen extends Render {
         }
 
         render.floor(game);
+        render.renderDistanceLimiter();
         draw(render, 0, 0);
     }
 }
